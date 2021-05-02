7c5fe15ef315f78e7a1f4180c115910978b84c49
JanaBobulis <jana.bobulis@hotmail.com>
2021-05-02 12:00:13 +0100

    js file

---
+++ JS/index.js
@@ -0,0 +1,35 @@
+function openNav() {
+    let overlayContent = document.getElementById('overlay');
+    let openCloseBtn = document.getElementsByClassName('openBtn');
+    let xIcon = document.querySelector(".xIcon");
+    let menuIcon = document.querySelector(".menuIcon");
+
+    overlayContent.classList.contains("showMenu");
+
+    if(overlayContent.classList.contains("showMenu")) {
+      overlayContent.classList.remove("showMenu");
+      xIcon.style.display = "none";
+      menuIcon.style.display = "block"
+    } else {
+      overlayContent.classList.add("showMenu");
+      xIcon.style.display = "block";
+      xIcon.style.color = "hsl(0, 0%, 63%)"
+      menuIcon.style.display = "none"
+    }
+
+    if(overlayContent.style.display === "flex") {
+      overlayContent.style.display = "none"
+    } else {
+      overlayContent.style.display = "flex"
+    }
+  }
+
+  let menuLinks = document.querySelectorAll(".menuLink");
+
+  menuLinks.forEach(
+    function(menuLink) {
+      menuLink.addEventListener("click", openNav());
+    }
+  )
+
+
