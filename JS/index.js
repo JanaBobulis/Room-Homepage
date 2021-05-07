d21e23d5c4bf2be6eb56d7fcca84d1df0a97af06
JanaBobulis <jana.bobulis@hotmail.com>
2021-05-07 13:32:56 +0100

    navigation menu smooth transition

--- JS/index.js
+++ JS/index.js
@@ -10,6 +10,39 @@ function openNav() {
       overlayContent.classList.remove("showMenu");
       xIcon.style.display = "none";
       menuIcon.style.display = "block"
+      overlayContent.style.opacity = "0"
+    } else {
+      overlayContent.classList.add("showMenu");
+      xIcon.style.display = "block";
+      xIcon.style.color = "hsl(0, 0%, 63%)"
+      menuIcon.style.display = "none"
+      overlayContent.style.opacity = "1"
+    }
+
+  }
+
+  let menuLinks = document.querySelectorAll(".menuLink");
+
+  menuLinks.forEach(
+    function(menuLink) {
+      menuLink.addEventListener("click", openNav());
+    }
+  )
+
+
+  /*
+  function openNav() {
+    let overlayContent = document.getElementById('overlay');
+    let openCloseBtn = document.getElementsByClassName('openBtn');
+    let xIcon = document.querySelector(".xIcon");
+    let menuIcon = document.querySelector(".menuIcon");
+
+    overlayContent.classList.contains("showMenu");
+
+    if(overlayContent.classList.contains("showMenu")) {
+      overlayContent.classList.remove("showMenu");
+      xIcon.style.display = "none";
+      menuIcon.style.display = "block"
     } else {
       overlayContent.classList.add("showMenu");
       xIcon.style.display = "block";
@@ -31,5 +64,5 @@ function openNav() {
       menuLink.addEventListener("click", openNav());
     }
   )
-
+  */
 
