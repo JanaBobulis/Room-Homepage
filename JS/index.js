ef7f67fd22fb786b8755a10a75e4b873a3229f93
Jana Bobulis <71527795+JanaBobulis@users.noreply.github.com>
2021-05-20 14:32:55 +0100

    desktop nav menu

--- JS/index.js
+++ JS/index.js
@@ -4,6 +4,7 @@ function openNav() {
     let xIcon = document.querySelector(".xIcon");
     let menuIcon = document.querySelector(".menuIcon");
     let container = document.querySelector('body')
+    
 
     overlayContent.classList.contains("showMenu");
     container.classList.toggle("open-menu")
@@ -61,3 +62,30 @@ function showSlides(n) {
 }
 
 
+function desktopDisplay() {
+  if (document.documentElement.clientWidth >= 924) {
+    document.getElementById("navigation").className = "desktopNav"
+    document.getElementById("overlay").className = "desktopOverlay"
+  } 
+}
+
+window.onresize = desktopDisplay;
+
+desktopDisplay()
+
+
+/*
+function desktopDisplay() {
+  let navMenu = document.getElementById('navigation')
+  let overlayContent = document.getElementById('overlay');
+  let navLink = document.getElementById("navLink")
+  if (document.documentElement.clientWidth >= 924) {
+    navMenu.style.position = "relative";
+    overlayContent.style.backgroundColor = "transparent"
+    overlayContent.style.opacity = "1"
+    navLink.style.color = "white";
+    navLink.style.fontSize = "20px"
+    overlayContent.style.justifyContent = "center"
+  } 
+}
+*/
