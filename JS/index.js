5bf58ebe5bb0bbe4e913ad4f18f464b0a14c6301
Jana Bobulis <71527795+JanaBobulis@users.noreply.github.com>
2021-05-21 10:17:41 +0100

    code tidying

--- JS/index.js
+++ JS/index.js
@@ -1,37 +1,37 @@
 function openNav() {
-    let overlayContent = document.getElementById('overlay');
-    let openCloseBtn = document.getElementsByClassName('openBtn');
-    let xIcon = document.querySelector(".xIcon");
-    let menuIcon = document.querySelector(".menuIcon");
-    let container = document.querySelector('body')
-    
+  let overlayContent = document.getElementById('overlay');
+  let openCloseBtn = document.getElementsByClassName('openBtn');
+  let xIcon = document.querySelector(".xIcon");
+  let menuIcon = document.querySelector(".menuIcon");
+  let container = document.querySelector('body')
 
-    overlayContent.classList.contains("showMenu");
-    container.classList.toggle("open-menu")
 
-    if(overlayContent.classList.contains("showMenu")) {
-      overlayContent.classList.remove("showMenu");
-      xIcon.style.display = "none";
-      menuIcon.style.display = "block"
-      overlayContent.style.opacity = "0"
-      container.classList.remove("open-menu")
+  overlayContent.classList.contains("showMenu");
+  container.classList.toggle("open-menu")
 
-    } else {
-      overlayContent.classList.add("showMenu");
-      xIcon.style.display = "block";
-      xIcon.style.color = "hsl(0, 0%, 63%)"
-      menuIcon.style.display = "none"
-      overlayContent.style.opacity = "1";  
-    }
+  if (overlayContent.classList.contains("showMenu")) {
+    overlayContent.classList.remove("showMenu");
+    xIcon.style.display = "none";
+    menuIcon.style.display = "block"
+    overlayContent.style.opacity = "0"
+    container.classList.remove("open-menu")
+
+  } else {
+    overlayContent.classList.add("showMenu");
+    xIcon.style.display = "block";
+    xIcon.style.color = "hsl(0, 0%, 63%)"
+    menuIcon.style.display = "none"
+    overlayContent.style.opacity = "1";
   }
+}
 
-  let menuLinks = document.querySelectorAll(".menuLink");
+let menuLinks = document.querySelectorAll(".menuLink");
 
-  menuLinks.forEach(
-    function(menuLink) {
-      menuLink.addEventListener("click", openNav());
-    }
-  )
+menuLinks.forEach(
+  function (menuLink) {
+    menuLink.addEventListener("click", openNav());
+  }
+)
 
 //slideshow
 let slideIndex = 1;
@@ -48,17 +48,17 @@ function currentSlides(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slides");
-  if(n > slides.length) {
+  if (n > slides.length) {
     slideIndex = 1;
-  } 
-  if(n < 1) {
+  }
+  if (n < 1) {
     slideIndex = slides.length;
   }
-  for(i = 0; i < slides.length; i++) {
+  for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
 
-  slides[slideIndex-1].style.display = "block"
+  slides[slideIndex - 1].style.display = "block"
 }
 
 
@@ -66,7 +66,7 @@ function desktopDisplay() {
   if (document.documentElement.clientWidth >= 924) {
     document.getElementById("navigation").className = "desktopNav"
     document.getElementById("overlay").className = "desktopOverlay"
-  } 
+  }
 }
 
 window.onresize = desktopDisplay;
